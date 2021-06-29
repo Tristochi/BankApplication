bcf2b14d05fce26d7065912ac6b69f15ed2b5e21
Tristochi <TristanLSzakacs@gmail.com>
2021-06-29 15:08:18 -0400

    added getters/setters

--- src/BankApplicationPkg/Customer.java
+++ src/BankApplicationPkg/Customer.java
@@ -6,11 +6,73 @@
  * Customer Phone - String
  * Customer Address - String
  */
+
 package BankApplicationPkg;
 
 public class Customer {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		
+	private int customerID;
+	private int pin;
+	private String custName;
+	private String custEmail;
+	private String custPhone;
+	private String custAddress;
+	
+	public Customer() {
+		customerID = 0;
+		pin = 0;
+		custName = "";
+		custEmail = "";
+		custPhone = "";
+		custAddress = "";
 	}
+	public Customer(int customerID, int pin, String custName, String custEmail, String custPhone, String custAddress) {
+		this.customerID = customerID;
+		this.pin = pin;
+		this.custAddress  = custAddress;
+		this.custEmail = custEmail;
+		this.custPhone = custPhone;
+		this.custAddress = custAddress;
+	}
+	
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID; 
+	}
+	
+	public void setCustomerEmail(String custEmail) {
+		this.custEmail= custEmail;
+	}
+	
+	public void setCustomerPhone(String custPhone) {
+		this.custPhone = custPhone;
+	}
+	
+	public void setCustomerAddress(String custAddress) {
+		this.custAddress = custAddress;
+	}
+	
+	public int getCustID() {
+		return customerID;
+	}
+	
+	public int getPin() {
+		return pin;
+	}
+	
+	public String getCustName() {
+		return custName;
+	}
+	
+	public String getCustEmail() {
+		return custEmail;
+	}
+	
+	public String getCustPhone() {
+		return custPhone;
+	}
+	
+	public String getCustAddress() {
+		return custAddress;
+	}
+	
+
 }
